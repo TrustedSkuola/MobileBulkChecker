c4f5fae4883869aec21f8e6fcc566f713c7871ed
Gianluca Benucci <gianluca.benucci@skuola.net>
2021-09-12 18:23:51 +0200

    Add dotenv for get api key from .env file, add control for check if an api key is set

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
-console.log("Hello world");
+const apiKey = process.argv[2];
 
-
+if (!apiKey) {
+	return console.log('\x1b[41m', 'Please enter api key, run: MobileBulkChecker [api-key]' ,'\x1b[0m');
+}
