8f79cfb5e341948aab16d3ffd6498c020985ee1c
Gianluca Benucci <gianluca.benucci@skuola.net>
2021-09-15 20:13:54 +0200

    Add minimist library and first version of script with cli input

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
+
 require('dotenv').config()
 require('esm')(module /*, options*/);
 
 import fetch from "node-fetch";
 
-async function callMobileFriendlyApi(apiKey, data = {}) {
+
+async function checkMobileFriendly(apiKey, data = {}) {
 
   const apiURL = "https://searchconsole.googleapis.com/v1/urlTestingTools/mobileFriendlyTest:run?key="+apiKey
   
@@ -19,24 +21,49 @@ async function callMobileFriendlyApi(apiKey, data = {}) {
 export function cli(args) {
 
   const apiKey = process.env.API_KEY;
-  const urlToCheck = process.argv[2];
+  const argv = require('minimist')(process.argv.slice(2));
 
-  if (!apiKey || apiKey === 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX') {
-    console.log('\x1b[34m', 'It seems that you have not set any API keys, AMP Test will be skipped' ,'\x1b[0m');
+  const url = argv.url;
+  const file = argv.file;
+  const mode = argv.mode;
+
+  const isValid = /^(http|https):\/\/[^ "]+$/.test(url);
+
+
+  if ( !apiKey || apiKey === 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' ) {
+    console.log('\x1b[41m', 'You forgot to set any API keys, this is a bad thing.' ,'\x1b[0m');
     process.exit()
   }
 
-  if (!urlToCheck) {
+  if ( !url ) {
+    console.log('\x1b[41m', 'You forgot to enter url to check, run: MobileBulkChecker --url=URL_TO_CHECK' ,'\x1b[0m');
+    process.exit()
+  }
+
+  if ( !isValid ) {
     console.log('\x1b[41m', 'You forgot to enter url to check, run: MobileBulkChecker [url]' ,'\x1b[0m');
     process.exit()
   }
-  
-  callMobileFriendlyApi(apiKey,{
-    "url": urlToCheck,
-    "requestScreenshot": false
-    }).then(data => {
-      console.log(data);
-    });
+
+  if ( url && file ){
+    console.log('\x1b[33m', 'Sorry dude but for now is only possible check url or file once at time' ,'\x1b[0m');
+    process.exit()
+  }
+
+  if ( url ){
+
+    checkMobileFriendly(apiKey,{
+      "url": url,
+      "requestScreenshot": false
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .then(data => {
+        console.log(data);
+      });
+
+  }
 
 
 }
